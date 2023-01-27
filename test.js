08629d64b7f0e3927b885e7836306074a133a139
ceonte brown <119247370+ceonte13@users.noreply.github.com>
2023-01-26 18:18:08 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,16 @@
-    let expect= chai.expect;
-describe("allcards", function() {
+   let expect= chai.expect;
     describe("allcards", function(){
         it("should combine suits and values", function(){
             let x = allCards(suits, values);
             expect(x).to.equal(suits + values);
-        });
+}
+    );
 
-        it("should throw an error  is not a string", function(){
+      it("should throw an error  not correct answer", function(){
             expect(function(){
                 combineCards(suit);
             }).to.throw(Error);
         });
     });
-
 
 
